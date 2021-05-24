c8ac50e49611d2559a9eb1008f74faf1ce5571b7
Hadi Abedzadeh <Hadi-Abedzadeh@users.noreply.github.com>
2021-05-24 09:44:22 +0430

    Update get data from mysql to chartjs.php

--- get data from mysql to chartjs.php
+++ get data from mysql to chartjs.php
@@ -1,4 +1,8 @@
 Refrence: https://www.reddit.com/r/laravel/comments/2xeqz2/laravel_and_eloquent_count_with_chartjs/
+and for ajax you can use this link: https://dyclassroom.com/chartjs/chartjs-how-to-create-line-graph-using-data-from-mysql-mariadb-table-and-php
+Google url was searched: https://www.google.com/search?rlz=1C1GCEA_enIR936IR936&sxsrf=ALeKk03n3HyeFFzwOsmVzcOkoHZ0r4FItw:1621799880602&q=get+data+mysql+by+date+day+to+show+in+chart+js&spell=1&sa=X&ved=2ahUKEwjQ-p2uy-DwAhXPOcAKHeHsCJ4QBSgAegQIARAt&biw=1920&bih=969
+keyword was searched: get data mysql by date day to show in chart js
+
 
 //Laravel ORM
 $start = '2021-04-12';
